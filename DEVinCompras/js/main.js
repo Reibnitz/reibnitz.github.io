cafaf7a2a861986b3f8a59ff31987581c78d9173
Reibnitz <97990727+Reibnitz@users.noreply.github.com>
2022-04-12 19:00:51 -0300

    Delete DEVinCompras directory

--- DEVinCompras/js/main.js
+++
@@ -1,6 +0,0 @@
-import { Controller } from "./Controller.js";
-
-export let controller = new Controller();
-
-
-document.querySelector('#input-container').addEventListener('submit',(event) => controller.submit(event));
